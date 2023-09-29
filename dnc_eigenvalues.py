6e8b1b1481dc9b4d4a515a6e4de0b0f751701b74
lollerfirst <lollerfirst@gmail.com>
2023-09-29 16:38:04 +0200

    Completed solve_base

--- dnc_eigenvalues.py
+++ dnc_eigenvalues.py
@@ -1,19 +1,50 @@
 import numpy as np
 import random
-from math import abs
+from math import abs, sqrt
 
+
+# machine precision
 epsilon = 1e-10
+
+def solve_base(A):
+    # det(A-lI) = 0 => (A[0,0]-l)*(A[1,1]-l)-A[1,0]*A[0,1] = 0
+    a = A[0,0], b = A[0,1], c = A[1,0], d = A[1,1]
+
+    eig = np.array([((a+d) + sqrt((a+d)**2 - 4 * (a*d-b*c))) / 2,
+                    ((a+d) - sqrt((a+d)**2 - 4 * (a*d-b*c))) / 2])
+    Q = np.empty((2,2))
+
+    for i in range(2):
+        F = np.copy(A)
+        F[0,0] -= eig[i]
+        F[1,1] -= eig[i]
+
+        # Mini Gaussian Elimination
+        k = F[1,0] / F[0,0]
+        F[1,:] = -k * F[0,:] + F[1,:]
+        
+        if abs(F[1,1]) > epsilon:
+        
+            Q[1,i] = 0
+            Q[0,i] = 0
+        
+        else:
+            Q[1,i] = 1
+            Q[0,i] = -F[0,(i+1)&1] / F[0, i]
+
+    return Q, eig
+    
 
 def psi(v, d, l, start, stop):
     x = 0
-    for k in range(start, stop):
+    for k in range(start, stop+1):
         x += (v[k]**2) / (d[start] - l)
     return x
 
 # derivative of psi wrt l (the eigenvalue)
 def dpsi_dl(v, d, l, start, stop):
     x = 0
-    for k in range(start, stop):
+    for k in range(start, stop+1):
         x += (v[k]**2) / ((d[start] - l)**2)
     return x
 
@@ -51,9 +82,13 @@ def dnc_eigenvalues(T: np.matrix):
 
     [d, v] = sorted(zip(d, v), lambda x: x[0]) # Sort the diagonal values
 
-    # (D + bzz^T)v = 0 ==> 1 + bz^T(D - vI)^-1 = 0 ==> 1 - b SUM_k=1^n (z_k^2 / (v - d_k)) = 0
+    # (D + bvv^T)l = 0 ==> 1 + bv^T(D - lI)^-1 = 0 ==> 1 - b SUM_k=1^n (v_k^2 / (l - d_k)) = 0
     eig = np.empty(n)
     X = np.empty((n,n))
+
+    # TODO introduce deflation:
+    #    - Checking of zeros in v^T
+    #    - Apply Givens' rotations to v^T and D to form zeros
 
     for i in range(n):
 
@@ -79,9 +114,9 @@ def dnc_eigenvalues(T: np.matrix):
             c_1 = tmp*((d[i] - l)**2)
             c_1_hat = b*psi(v, d, l, 0, i) - tmp*(d[i] - l)         # (5.28)
             
-            tmp = b*dpsi_dl(v, d, l, i+1, n)
+            tmp = b*dpsi_dl(v, d, l, i+1, n-1)
             c_2 = tmp*((d[i+1] - l)**2)
-            c_2_hat = b*psi(v, d, l, i+1, n) - tmp*(d[i+1] - l)     # (5.29)
+            c_2_hat = b*psi(v, d, l, i+1, n-1) - tmp*(d[i+1] - l)     # (5.29)
 
             c_3 = 1 + c_1_hat + c_2_hat
             err = c_3 + c_1 / (d[i] - l) + c_2 / (d[i+1] - l)       # (5.30) see also (5.25) (5.26)
